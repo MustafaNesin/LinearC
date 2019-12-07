963ab0df60fd3d627e5343ff92bc3f7de3826624
Mustafa Nesin <mustafa.nesin@gazi.edu.tr>
2019-12-07 23:01:14 +0300

    Matris toplama fonksiyonunun adı prototipe göre düzeltildi.

--- Matrix.c
+++ Matrix.c
@@ -1,7 +1,7 @@
 #include "Matrix.h"
 #include <stdlib.h>
 
-Matrix* matrix_addition(Matrix* matrix1, Matrix* matrix2)
+Matrix* mx_add(Matrix* matrix1, Matrix* matrix2)
 {
 	if (!matrix1 || !matrix2)
 		return 0;
