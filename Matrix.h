078c07552fac3a3b05e49431ae774d3d7fb59c16
Mustafa Nesin <mustafa.nesin@gazi.edu.tr>
2019-12-07 20:19:54 +0300

    Row count and column counts are integer types.

--- Matrix.h
+++ Matrix.h
@@ -1,11 +1,11 @@
 #pragma once
 typedef struct Matrix
 {
-	short rowCount;
-	short columnCount;
+	int rowCount;
+	int columnCount;
 	float* data;
 } Matrix;
 
-// Parametre olarak verilen iki matrisi toplar ve döndürür.
+// Parametre olarak verilen iki matrisi toplar ve sonucu döndürür.
 // Eðer matrislerin boyutu birbirine eþit deðilse NULL (0) döndürür.
 Matrix* matrix_addition(Matrix*, Matrix*);
