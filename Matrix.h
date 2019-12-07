97c06fd78e3f2af92896b3f9948cb523e180f7c2
Mustafa Nesin <mustafa.nesin@gazi.edu.tr>
2019-12-07 22:58:22 +0300

    Bazı matris işlemleri fonksiyonlarının prototipleri oluşturuldu.

--- Matrix.h
+++ Matrix.h
@@ -6,6 +6,16 @@ typedef struct Matrix
 	float* data;
 } Matrix;
 
-// Parametre olarak verilen iki matrisi toplar ve sonucu döndürür.
+// Verilen iki matrisi toplamýný döndürür.
 // Eðer matrislerin boyutu birbirine eþit deðilse NULL (0) döndürür.
-Matrix* matrix_addition(Matrix*, Matrix*);
+Matrix* mx_add(Matrix*, Matrix*);
+
+// Verilen sayý ile matrisin çarpýmýný döndürür.
+Matrix* mx_sdot(float, Matrix*);
+
+// Verilen iki matrisin çarpýmýný döndürür.
+// Eðer birinci matrisin sütun sayýsý ikinci matrisin satýr sayýna eþit deðilse NULL (0) döndürür.
+Matrix* mx_dot(Matrix*, Matrix*);
+
+// Verilen matrisin transpozunu döndürür.
+Matrix* mx_t(Matrix*);
