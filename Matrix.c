2182c6b1c16a28cbf23de03ad141d367d8a21eb1
Cem Ufuk YILMAZ <cemufuk.yilmaz@gazi.edu.tr>
2019-12-09 15:41:04 +0300

    Bu değişiklik geri alınacak.

--- Matrix.c
+++ Matrix.c
@@ -91,6 +91,7 @@ Matrix* mx_sdot(float scalar, Matrix* matrix)
 		*(result->data + i) = *(matrix->data + i) *  scalar;
 }
 
+//test
 Matrix* mx_t(Matrix* matrix)
 {
 	Matrix* result = malloc(sizeof(Matrix));
